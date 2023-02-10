2f7b451b4b9a695785de431f3b418be5be56a090
Raymond Camden <raymondcamden@gmail.com>
2023-02-10 11:44:31 -0600

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -11,7 +11,7 @@ module.exports = (eleventyConfig, options) => {
 
 		return `
 <div id="${divid}"></div>
-<script src="https://documentcloud.adobe.com/view-sdk/main.js"></script>
+<script src="https://documentservices.adobe.com/view-sdk/viewer.js"></script>
 <script type="text/javascript">
 	document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
 		var adobeDCView = new AdobeDC.View({clientId: "${options.key}", divId: "${divid}"});
